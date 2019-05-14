802dbb4c4d0b6f09d03fed6453b2a59ef4f9593a
Álvaro Toro <torom.alvaro@gmail.com>
2019-05-13 21:09:24 -0500

    add files

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var NavBar = require("./components/NavBar.jsx");
+
+ReactDOM.render(<NavBar/>, document.getElementById("main"));
+
+
+
